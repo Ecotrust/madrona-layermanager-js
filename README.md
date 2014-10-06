3d53f2aa32a2d3577d76b2d71086d5e91a734c30
Matthew Perry <perrygeo@gmail.com>
2014-10-06 11:23:02 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Madrona Layer Manager JS
+
+A pure Javascript/HTML version of the madrona 2D layer manager. No server requirements.
+
+### Adding layers
+
+Edit `js/demo_fixture.js` to add/remove layers and organize them into groups.
---
+++ js/demo_fixture.js
@@ -0,0 +1,168 @@
+app.fixture = {
+  "layers": [
+    {
+      "opacity": 1.0, 
+      "legend_subtitle": null, 
+      "lookups": {
+        "field": null, 
+        "details": []
+      }, 
+      "graphic": null, 
+      "name": "CloudMade Web", 
+      "url": [
+        "http://a.tile.cloudmade.com/8fe4ccbb4940415d9475cc21bf41ea53/1/256/${z}/${x}/${y}.png", 
+        "http://b.tile.cloudmade.com/8fe4ccbb4940415d9475cc21bf41ea53/1/256/${z}/${x}/${y}.png", 
+        "http://c.tile.cloudmade.com/8fe4ccbb4940415d9475cc21bf41ea53/1/256/${z}/${x}/${y}.png"
+      ], 
+      "type": "XYZ", 
+      "arcgis_layers": null, 
+      "default_on": false, 
+      "legend_title": null, 
+      "data_source": "2011 CloudMade", 
+      "subLayers": [], 
+      "id": 1, 
+      "attributes": {
+        "attributes": [], 
+        "compress_attributes": false, 
+        "event": "click", 
+        "title": null
+      }, 
+      "fill_opacity": null, 
+      "learn_link": null, 
+      "utfurl": null, 
+      "legend": null, 
+      "color": null, 
+      "description": null
+    }, 
+    {
+      "opacity": 1.0, 
+      "legend_subtitle": null, 
+      "lookups": {
+        "field": null, 
+        "details": []
+      }, 
+      "graphic": null, 
+      "name": "Argyle", 
+      "url": [
+        "http://temp1.argyl.es/preview-key/${z}/${x}/${y}.jpg", 
+        "http://temp2.argyl.es/preview-key/${z}/${x}/${y}.jpg", 
+        "http://temp3.argyl.es/preview-key/${z}/${x}/${y}.jpg"
+      ], 
+      "type": "XYZ", 
+      "arcgis_layers": null, 
+      "default_on": false, 
+      "legend_title": null, 
+      "data_source": "Nathan Vander Wilt", 
+      "subLayers": [], 
+      "id": 2, 
+      "attributes": {
+        "attributes": [], 
+        "compress_attributes": false, 
+        "event": "click", 
+        "title": null
+      }, 
+      "fill_opacity": null, 
+      "learn_link": null, 
+      "utfurl": null, 
+      "legend": null, 
+      "color": null, 
+      "description": null
+    }, 
+    {
+      "opacity": 1.0, 
+      "legend_subtitle": null, 
+      "lookups": {
+        "field": null, 
+        "details": []
+      }, 
+      "graphic": null, 
+      "name": "World Blue", 
+      "url": [
+        "http://a.tiles.mapbox.com/v3/mapbox.world-blue/${z}/${x}/${y}.png", 
+        "http://b.tiles.mapbox.com/v3/mapbox.world-blue/${z}/${x}/${y}.png", 
+        "http://c.tiles.mapbox.com/v3/mapbox.world-blue/${z}/${x}/${y}.png", 
+        "http://d.tiles.mapbox.com/v3/mapbox.world-blue/${z}/${x}/${y}.png"
+      ], 
+      "type": "XYZ", 
+      "arcgis_layers": null, 
+      "default_on": false, 
+      "legend_title": null, 
+      "data_source": "Mapbox", 
+      "subLayers": [], 
+      "id": 3, 
+      "attributes": {
+        "attributes": [], 
+        "compress_attributes": false, 
+        "event": "click", 
+        "title": null
+      }, 
+      "fill_opacity": null, 
+      "learn_link": null, 
+      "utfurl": null, 
+      "legend": null, 
+      "color": null, 
+      "description": null
+    }, 
+    {
+      "opacity": 1.0, 
+      "legend_subtitle": null, 
+      "lookups": {
+        "field": null, 
+        "details": []
+      }, 
+      "graphic": null, 
+      "name": "Nokia Hybrid", 
+      "url": [
+        "http://b.maptile.maps.svc.ovi.com/maptiler/v2/maptile/newest/hybrid.day/${z}/${x}/${y}/256/png8", 
+        "http://c.maptile.maps.svc.ovi.com/maptiler/v2/maptile/newest/hybrid.day/${z}/${x}/${y}/256/png8", 
+        "http://d.maptile.maps.svc.ovi.com/maptiler/v2/maptile/newest/hybrid.day/${z}/${x}/${y}/256/png8", 
+        "http://e.maptile.maps.svc.ovi.com/maptiler/v2/maptile/newest/hybrid.day/${z}/${x}/${y}/256/png8"
+      ], 
+      "type": "XYZ", 
+      "arcgis_layers": null, 
+      "default_on": false, 
+      "legend_title": null, 
+      "data_source": "Nokia", 
+      "subLayers": [], 
+      "id": 4, 
+      "attributes": {
+        "attributes": [], 
+        "compress_attributes": false, 
+        "event": "click", 
+        "title": null
+      }, 
+      "fill_opacity": null, 
+      "learn_link": null, 
+      "utfurl": null, 
+      "legend": null, 
+      "color": null, 
+      "description": null
+    }
+  ], 
+  "themes": [
+    {
+      "layers": [
+        1, 
+        2
+      ], 
+      "learn_link": "", 
+      "display_name": "Category A", 
+      "description": null, 
+      "id": 52
+    }, 
+    {
+      "layers": [
+        3, 
+        4 
+      ], 
+      "learn_link": "", 
+      "display_name": "Category B", 
+      "description": null, 
+      "id": 54
+    }, 
+  ],
+  "state": {
+    "activeLayers": []
+  }, 
+  "success": true
+}
